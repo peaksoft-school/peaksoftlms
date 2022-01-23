7f668f4e078d839d311ab1d5eaaa7d1b06110b21
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-01-24 00:21:53 +0600

    DB deploy

--- src/main/java/kg/peaksoftlms/peaksoftlms/PeaksoftlmsApplication.java
+++ src/main/java/kg/peaksoftlms/peaksoftlms/PeaksoftlmsApplication.java
@@ -6,25 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@OpenAPIDefinition
 public class PeaksoftlmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeaksoftlmsApplication.class, args);
-		System.out.println("Hey, Aza, Maks, Nurperi, Aida, the project name is Peaksoftlms");
-	}
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-//	@Bean
-//	CommandLineRunner commandLineRunner(UserRepository repository, RoleRepository roleRepository) {
-//		return args -> {
-//			Role role = new Role(null, "ROLE_ADMIN");
-//			Role role2 = new Role(null, "ROLE_STUDENT");
-//////
-//////			User user = new User(null, "max@gmail.com", "123", "Max", "Akyl",
-//////					null, null, null, null, null, Arrays.asList(role));
-//////			User user2 = new User(null, "nurperi@gmail.com", "123", "Nurperi", "Arzykulova",
-//////					null, null, null, null, null, Arrays.asList(role2));
-////
-////			repository.save(user);
-////			repository.save(user2);
-//		};
+    @PostConstruct
+    public void initUsers() {
+        List<User> users = Stream.of(
+                new User("admin@gmail.com", passwordEncoder.encode("qwerty"))
+        ).collect(Collectors.toList());
+        userRepository.saveAll(users);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeaksoftlmsApplication.class, args);
+    }
+
 }
