33c006fd2d6f0c75cdc158132aba88beaa95ca7d
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-01-24 16:52:10 +0600

    add @CrossOrigin, TestApi class, /api/test* permission

--- src/main/java/kg/peaksoftlms/peaksoftlms/config/security/WebSecurityConfig.java
+++ src/main/java/kg/peaksoftlms/peaksoftlms/config/security/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/authentication").permitAll()
+                .antMatchers( "/api/test/**").permitAll()
                 .antMatchers("/api/admin").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated();
         http.logout().logoutUrl("/api/logout") //URL-адрес, запускающий выход из системы (по умолчанию "/logout").
