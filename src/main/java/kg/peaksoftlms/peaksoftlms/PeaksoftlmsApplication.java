51898ff468f88fbd757a73705ee589b6f7bd3335
iman <imanaida135@gmail.com>
2022-01-28 21:43:44 +0300

    course-crud v5

--- src/main/java/kg/peaksoftlms/peaksoftlms/PeaksoftlmsApplication.java
+++ src/main/java/kg/peaksoftlms/peaksoftlms/PeaksoftlmsApplication.java
@@ -1,8 +1,18 @@
 package kg.peaksoftlms.peaksoftlms;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import kg.peaksoftlms.peaksoftlms.db.model.User;
+import kg.peaksoftlms.peaksoftlms.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @SpringBootApplication
@@ -13,13 +23,13 @@ public class PeaksoftlmsApplication {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @PostConstruct
-    public void initUsers() {
-        List<User> users = Stream.of(
-                new User("admin@gmail.com", passwordEncoder.encode("qwerty"))
-        ).collect(Collectors.toList());
-        userRepository.saveAll(users);
-    }
+//    @PostConstruct
+//    public void initUsers() {
+//        List<User> users = Stream.of(
+//                new User("admin@gmail.com", passwordEncoder.encode("qwerty"))
+//        ).collect(Collectors.toList());
+//        userRepository.saveAll(users);
+//    }
 
 
     public static void main(String[] args) {
