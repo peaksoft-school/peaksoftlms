342c5d573707d5f2856c1b5a0a62dc5d0de6a2e7
iman <imanaida135@gmail.com>
2022-02-25 15:23:55 +0300

    lesson Api v2

--- src/main/java/kg/peaksoftlms/peaksoftlms/db/model/lesson/Test.java
+++ src/main/java/kg/peaksoftlms/peaksoftlms/db/model/lesson/Test.java
@@ -1,37 +1,50 @@
-package kg.peaksoftlms.peaksoftlms.db.model.lesson;
-
-import kg.peaksoftlms.peaksoftlms.db.model.lesson.test.Question;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Set;
-
-@Entity
-@Table
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class Test {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    //private Set<Question> question;
-    private String answer;
-    //add
-    private int testMarksScored;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private LocalTime testDuration;
-
-    //    @OneToMany(fetch =LAZY, cascade = {DETACH, MERGE, PERSIST, REFRESH})
-    @ManyToOne
-    private Lesson lesson;
-}
+//package kg.peaksoftlms.peaksoftlms.db.model.lesson;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import kg.peaksoftlms.peaksoftlms.db.model.lesson.test.Question;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import org.hibernate.annotations.CreationTimestamp;
+//import org.springframework.data.annotation.CreatedDate;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//@Entity
+//@Table
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//public class Test {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String title;
+//    private String description;
+//    private int testCodeQuantity;
+//
+//    @CreatedDate
+//    @CreationTimestamp
+//    private LocalDateTime createdAt;
+//
+//    private LocalDateTime startTime;
+//    private LocalDateTime endTime;
+//
+//    @Column(name = "doing_duration")
+//    private int duration; // stored by minutes
+//
+//    //private int status; // 1: active, 0: inactive
+//
+//    @OneToMany
+//    private List<Question> question;
+//
+//
+//    //    @OneToMany(fetch =LAZY, cascade = {DETACH, MERGE, PERSIST, REFRESH})
+//    @ManyToOne
+//    @JsonIgnore
+//    private Lesson lesson;
+//}
--- src/main/java/kg/peaksoftlms/peaksoftlms/service/lesson/TestService.java
+++ src/main/java/kg/peaksoftlms/peaksoftlms/service/lesson/TestService.java
@@ -1,18 +1,20 @@
-package kg.peaksoftlms.peaksoftlms.service.lesson;
-
-import kg.peaksoftlms.peaksoftlms.db.dto.lesson.TaskRequest;
-import kg.peaksoftlms.peaksoftlms.db.model.lesson.Test;
-
-import java.util.List;
-
-public interface TestService {
-    List<Test> getAllByLessonId(Long lessonId);
-
-    Test getById(Long id);
-
-    Test saveNewTask(Test taskRequest, Long lessonId);
-
-    void deleteById(Long id);
-
-    void updateTask(Long id, TaskRequest taskRequest, Long lessonId);
-}
+//package kg.peaksoftlms.peaksoftlms.service.lesson;
+//
+//import kg.peaksoftlms.peaksoftlms.db.dto.lesson.test.QuestionRequest;
+//import kg.peaksoftlms.peaksoftlms.db.dto.lesson.test.TestRequest;
+//import kg.peaksoftlms.peaksoftlms.db.model.lesson.Test;
+//import kg.peaksoftlms.peaksoftlms.db.model.lesson.test.Question;
+//
+//public interface TestService {
+////    List<Test> getAllByLessonId(Long lessonId);
+////
+////    Test getById(Long id);
+//
+//    Test saveTest(TestRequest testRequest, Long lessonId);
+//
+//    public void saveUserAnswer(Long studentId, long testId, long questionId, long choiceId)
+//
+////    void deleteById(Long id);
+////
+////    void updateTask(Long id, TaskRequest taskRequest, Long lessonId);
+//}
